7803bed30797ce37e37398c1b80361acf45679df
Lim Joe Quan <joequanlim@gmail.com>
2025-12-20 22:52:10 +0800

    Write psuedocode/general plan for js

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # rock-paper-scissors
-Rock paper scissors project based on The Odin Project curriculum
+Rock paper scissors project based on The Odin Project curriculum. This project involves the use of JavaScript for game logic. UI is added later after progressing further through the curriculum.
---
+++ javascript.js
@@ -0,0 +1,41 @@
+// Write function called getComputerChoice
+// RNG number from 1 to 3
+// 1 = rock ; 2 = paper ; 3 = scissors
+
+
+
+
+
+
+
+
+// Write function called getHumanChoice
+// Use prompt(question, default) to get choice
+
+
+
+
+
+
+
+
+
+
+
+// Make var humanScore and computerScore (0 at first)
+// Write function called playRound with parameters humanChoice and computerChoice
+// Make humanChoice case-insensitive
+// playRound should log result stating winnner
+// ++ score of winner
+
+
+
+
+
+
+
+
+
+
+
+// Write playGame function that repeats this for 5 rounds (repeat 4 more times)
